a1fe38f42e03daf91179986f21ae4041aecfa80d
GonyaDev <gonyadev@yandex.ru>
2022-08-28 19:49:05 +0300

    Adding image of Uncle Ho to Login Screen

--- Chengyu/ViewController.swift
+++ Chengyu/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let loginHo: UIImageView = {
+        let logo = UIImageView()
+        logo.image = UIImage(named: "loginHo")
+        logo.contentMode = .scaleAspectFit
+        return logo
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(loginHo)
+        setupLayout()
     }
-
-
+    
+    func setupLayout() {
+        loginHo.translatesAutoresizingMaskIntoConstraints = false
+        
+        loginHo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginHo.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        loginHo.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        loginHo.heightAnchor.constraint(equalToConstant: 250).isActive = true
+    }
 }
 
